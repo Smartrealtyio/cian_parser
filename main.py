e6eae95f0b67b90e252b091a8161909371c9c212
Egor14 <egorgrekhnev@mail.ru>
2019-11-01 19:19:31 +0300

    CianParser class

--- main.py
+++ main.py
@@ -1,142 +1,235 @@
 import requests
 import json
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
-}
-
-url = 'https://www.cian.ru/sale/flat/194326031/'
-
-# page = requests.get(url, headers = headers).text
-
-# with open('page.html', 'w') as f:
-#     f.write(page)
-
-with open('page.html') as f:
-    page = f.read()
-
-# ---------------------------------------------
-# query
-
-yand_api_token = '31a6ed51-bc46-4d1d-9ac9-e3c2e22d2628'
-
+from datetime import datetime
 from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(page, 'lxml')
-
-address = soup.find('div', {'class': 'a10a3f92e9--geo--18qoo'}).find('span').get('content').split(',')
-address = [i.strip() for i in address]
-print(address)
-
-metros = list(map(lambda x: x.find('a').text, soup.find_all('li', {'class': 'a10a3f92e9--underground--kONgx'})))
-print(metros)
-
-main_info_response = soup.find_all('div', {'class': 'a10a3f92e9--info--2ywQI'})
-main_info = {}
-for info in main_info_response:
-    main_info.update({
-        info.find('div', {'class': 'a10a3f92e9--info-title--mSyXn'}).text: info.find('div', {
-            'class': 'a10a3f92e9--info-text--2uhvD'}).text
-    })
-print(main_info)
-
-general_info_response = soup.find_all('li', {'class': 'a10a3f92e9--item--_ipjK'})
-general_info = {}
-for info in general_info_response:
-    general_info.update({
-        info.find('span', {'class': 'a10a3f92e9--name--3bt8k'}).text: info.find('span', {
-            'class': 'a10a3f92e9--value--3Ftu5'}).text
-    })
-print(general_info)
-
-building_info_response = soup.find_all('div', {'class': 'a10a3f92e9--item--2Ig2y'})
-building_info = {}
-for info in building_info_response:
-    building_info.update({
-        info.find('div', {'class': 'a10a3f92e9--name--22FM0'}).text: info.find('div', {
-            'class': 'a10a3f92e9--value--38caj'}).text
-    })
-print(building_info)
-
-real_price = soup.find('span', {'class': 'a10a3f92e9--price_value--1iPpd'}).find('span').text
-print(real_price)
-
-history_prices_response = soup.find_all('tr', {'class': 'price_history_widget-history-event-nK20eRdJ'})
-history_prices = {}
-for info in history_prices_response:
-    history_prices.update({
-        info.find('td', {'class': 'price_history_widget-event-date-At3o0vWR'}).text:
-        info.find('td', {'class': 'price_history_widget-event-price-1hxoWz1dS'}).text
-    })
-print(history_prices)
-
-update_time = soup.find('div', {'class': 'a10a3f92e9--container--3nJ0d'}).text
-print(update_time)
-
-offer_id = url.split('/')[-2]
-print(offer_id)
+import time
 
 
-coords_response = requests.get(
-    f'https://geocode-maps.yandex.ru/1.x/?apikey={yand_api_token}&format=json&geocode={"".join(address)}').text
-coords = json.loads(coords_response)['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-longitude, latitude = coords.split(' ')
-print(longitude)
-print(latitude)
+class CianParser():
+    yand_api_token = '31a6ed51-bc46-4d1d-9ac9-e3c2e22d2628'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
+    }
+    street_names = {
+        'ул.': 'улица',
+        'пер.': 'переулок',
+        'ш.': 'шоссе',
+        'просп.': 'проспект',
+        'бул.': 'бульвар'
+    }
+    months = {
+        'января': 1,
+        'февраля': 2,
+        'марта': 3,
+        'апреля': 4,
+        'мая': 5,
+        'июня': 6,
+        'июля': 7,
+        'августа': 8,
+        'сентября': 9,
+        'октября': 10,
+        'ноября': 11,
+        'декабря': 12
+    }
+    building_types = {
+        'Деревянный': 'WOOD',
+        'Кирпичный': 'BRICK',
+        'Монолитный': 'MONOLIT',
+        'Панельный': 'PANEL',
+        'Монолитно-кирпичный': 'MONOLIT_BRICK',
+        'Блочный': 'BLOCK'
+    }
 
-# preparation
-street_names = {
-    'ул.': 'улица',
-    'пер.': 'переулок',
-    'ш.': 'шоссе',
-    'просп.': 'проспект',
-    'бул.': 'бульвар'
-}
+    def __init__(self, start_url):
+        self.url = start_url
 
-city = address[0]
-district = address[2].split(' ')[1]
-street = []
-for i in address[3].split(' '):
-    if i in street_names:
-        street.append(street_names[i])
-    else:
-        street.append(i)
-street = ' '.join(street)
-house_number = address[-1]
-print(city, district, street, house_number)
+    def parse_flat_info(self, url):
+        page = requests.get(url, headers=self.headers).text
+        soup = BeautifulSoup(page, 'lxml')
 
-full_sq = main_info['Общая'].split(' ')[0].replace(',', '.')
-kitchen_sq = -1
-life_sq = -1
-if 'Жилая' in main_info:
-    life_sq = main_info['Жилая'].split(' ')[0].replace(',', '.')
-if 'Кухня' in main_info:
-    kitchen_sq = main_info['Кухня'].split(' ')[0].replace(',', '.')
+        address = soup.find('div', {'class': 'a10a3f92e9--geo--18qoo'}).find('span').get('content').split(',')
+        address = [i.strip() for i in address]
 
-floor = main_info['Этаж'].split(' ')[0]
-max_floor = main_info['Этаж'].split(' ')[-1]
-flats_count = -1
-built_year = -1
-if 'Построен' in main_info:
-    built_year = main_info['Построен']
-elif 'Год постройки' in building_info:
-    built_year = building_info['Год постройки']
+        metros_response = soup.find_all('li', {'class': 'a10a3f92e9--underground--kONgx'})
+        metros = {}
+        for metro in metros_response:
+            metros.update({
+                metro.find('a').text: metro.find('span').text
+            })
 
-is_apartment = True
+        main_info_response = soup.find_all('div', {'class': 'a10a3f92e9--info--2ywQI'})
+        main_info = {}
+        for info in main_info_response:
+            main_info.update({
+                info.find('div', {'class': 'a10a3f92e9--info-title--mSyXn'}).text: info.find('div', {
+                    'class': 'a10a3f92e9--info-text--2uhvD'}).text
+            })
 
-renovation = False
-if 'Ремонт' in general_info:
-    renovation = True
+        general_info_response = soup.find_all('li', {'class': 'a10a3f92e9--item--_ipjK'})
+        general_info = {}
+        for info in general_info_response:
+            general_info.update({
+                info.find('span', {'class': 'a10a3f92e9--name--3bt8k'}).text: info.find('span', {
+                    'class': 'a10a3f92e9--value--3Ftu5'}).text
+            })
 
-building_type_str = 'UNKNOWN'
-if 'Тип дома' in building_info:
-    building_type_str = building_info['Тип дома']
+        building_info_response = soup.find_all('div', {'class': 'a10a3f92e9--item--2Ig2y'})
+        building_info = {}
+        for info in building_info_response:
+            building_info.update({
+                info.find('div', {'class': 'a10a3f92e9--name--22FM0'}).text: info.find('div', {
+                    'class': 'a10a3f92e9--value--38caj'}).text
+            })
 
-has_elevator = False
-if 'Лифты' in building_info:
-    has_elevator = building_info['Лифты']
+        real_price = soup.find('span', {'class': 'a10a3f92e9--price_value--1iPpd'}).find('span').text
+
+        history_prices_response = soup.find_all('tr', {'class': 'price_history_widget-history-event-nK20eRdJ'})
+        history_prices = {}
+        for info in history_prices_response:
+            history_prices.update({
+                info.find('td', {'class': 'price_history_widget-event-date-At3o0vWR'}).text:
+                    info.find('td', {'class': 'price_history_widget-event-price-1hxoWz1dS'}).text
+            })
+
+        update_time = soup.find('div', {'class': 'a10a3f92e9--container--3nJ0d'}).text
+
+        offer_id = url.split('/')[-2]
+
+        coords_response = requests.get(
+            f'https://geocode-maps.yandex.ru/1.x/?apikey={self.yand_api_token}&format=json&geocode={"".join(address)}').text
+        coords = json.loads(coords_response)['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point'][
+            'pos']
+        longitude, latitude = coords.split(' ')
+
+        # -------------------
+        # preparation
+
+        for metro, data in metros.items():
+            metros.update({
+                metro: {
+                    'time_to_metro': sum([int(i) if i.isdigit() else 0 for i in data.split(' ')]),
+                    'transport_type': 'ON_FOOT' * int('пешком' in data.split(' ')) or 'ON_TRANSPORT'
+                }
+            })
+
+        city = address[0]
+        district = address[-3].split(' ')[1]
+        street = []
+        for i in address[-2].split(' '):
+            if i in self.street_names:
+                street.append(self.street_names[i])
+            else:
+                street.append(i)
+        street = ' '.join(street)
+        house_number = address[-1]
+
+        full_sq = float(main_info['Общая'].split(' ')[0].replace(',', '.'))
+        kitchen_sq = -1
+        life_sq = -1
+        if 'Жилая' in main_info:
+            life_sq = float(main_info['Жилая'].split(' ')[0].replace(',', '.'))
+        if 'Кухня' in main_info:
+            kitchen_sq = float(main_info['Кухня'].split(' ')[0].replace(',', '.'))
+
+        floor = int(main_info['Этаж'].split(' ')[0])
+        max_floor = int(main_info['Этаж'].split(' ')[-1])
+        flats_count = -1
+        built_year = -1
+        if 'Построен' in main_info:
+            built_year = int(main_info['Построен'])
+        elif 'Год постройки' in building_info:
+            built_year = int(building_info['Год постройки'])
+
+        is_apartment = True
+        closed = False
+
+        renovation = False
+        if 'Ремонт' in general_info:
+            renovation = True
+
+        building_type_str = 'UNKNOWN'
+        if 'Тип дома' in building_info:
+            if building_info['Тип дома'] in self.building_types:
+                building_type_str = self.building_types[building_info['Тип дома']]
+
+        has_elevator = False
+        if 'Лифты' in building_info:
+            has_elevator = True
+
+        real_price = [datetime.now(), int(''.join(real_price.split()[:-1]))]
+        prices = []
+        year = 2019
+        for date, price in history_prices.items():
+            date = date.split(' ')
+            if date[0] == 'сегодня':
+                day = datetime.now().day
+                month = datetime.now().month
+            else:
+                day = int(date[0])
+                month = int(self.months[date[1]])
+            time = date[-1]
+            if ':' in time:
+                hours = int(date[-1].split(':')[0])
+                minutes = int(date[-1].split(':')[1])
+                prices.append([datetime(year, month, day, hours, minutes), int(''.join(price.split()[:-1]))])
+            else:
+                year = int(time)
+                prices.append([datetime(year, month, day), int(''.join(price.split()[:-1]))])
+        prices.append(real_price)
+
+        if address[0] == 'Москва':
+            address = ', '.join(['Россия', city, street, house_number])
+        else:
+            address = ', '.join(['Россия', address[0], address[1], street, house_number])
+
+        longitude = float(longitude)
+        latitude = float(latitude)
+
+        print(update_time, offer_id, district, full_sq, kitchen_sq, life_sq, floor, max_floor, building_type_str,
+              built_year, renovation, has_elevator, prices, metros, sep='\n')
+
+        return address
 
 
+    def get_flats_url(self, url):
+        response = requests.get(url, self.headers).text
+        soup = BeautifulSoup(response, 'lxml')
+        pages_response = soup.find_all('a', {'class': 'c6e8ba5398--header--1fV2A'})
+        pages_url = [page.get('href') for page in pages_response]
+        next_page_response = soup.find_all('li', {'class': '_93444fe79c--list-item--2KxXr'})
+        last_page_number = int(next_page_response[-1].find('a').text)
+        return pages_url, last_page_number
 
 
+    def get_setOfPages_url(self, url):
+        all_urls = []
+        page_number = 0
+        last_page_number = 2
+        url = url.format(page_number)
+        while (page_number < last_page_number):
+            page_number += 1
+            new_urls, last_page_number = self.get_flats_url(url)
+            all_urls += new_urls
+            url = url.format(page_number)
 
+        return all_urls
+
+
+    def parse(self):
+        urls = self.get_setOfPages_url(self.url)
+        print(*urls, sep='\n')
+        count = 0
+        for url in urls:
+            try:
+                print(self.parse_flat_info(url))
+                time.sleep(3)
+            except:
+                print('fail ', url)
+            count += 1
+
+
+        print('end ', count)
+
+
+parser = CianParser('https://www.cian.ru/cat.php?currency=2&deal_type=sale&engine_version=2&maxprice=35000000&offer_type=flat&p={}&region=1&room6=1')
+parser.parse()
